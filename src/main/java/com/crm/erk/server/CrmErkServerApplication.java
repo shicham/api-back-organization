5905adc4702494ef47b8dc9d651c8a4a813767b8
hicham <sass.hicham@gmail.com>
2020-12-20 09:19:59 +0100

    ala

--- src/main/java/com/crm/erk/server/CrmErkServerApplication.java
+++ src/main/java/com/crm/erk/server/CrmErkServerApplication.java
@@ -4,14 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class CrmErkServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmErkServerApplication.class, args);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public String getAuthor(@PathVariable("id") int id) {
+		return "id : " + id;
 	}
 
 }
