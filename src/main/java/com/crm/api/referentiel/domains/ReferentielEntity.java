8a2b54f8aade9a5e6a31ef4eca054a6c45dae1b0
HICHAM SASSA <hicham-externe.sassa@enedis.fr>
2020-12-21 16:05:43 +0100

    Update ReferentielEntity.java

--- src/main/java/com/crm/api/referentiel/domains/ReferentielEntity.java
+++ src/main/java/com/crm/api/referentiel/domains/ReferentielEntity.java
@@ -1,24 +1,20 @@
 package com.crm.api.referentiel.domains;
 
-import commun.domains.Domain;
+import com.crm.commun.domains.Domain;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "ref_referentiel")
 public class ReferentielEntity extends Domain {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(unique = true)
     private String internalCode;
     private String externalCode;
     private String name;
     private String description;
-    private Long parentId;
-    private ReferentielEntity parent;
 }
