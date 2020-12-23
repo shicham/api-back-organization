b692ecbb162495758ec9ebbcb048c7d2d1d616df
HICHAM SASSA <hicham-externe.sassa@enedis.fr>
2020-12-23 14:16:15 +0100

    Update OrganizationService.java

--- src/main/java/com/crm/api/organization/services/OrganizationService.java
+++ src/main/java/com/crm/api/organization/services/OrganizationService.java
@@ -10,9 +10,43 @@ import com.crm.commun.results.RequestFilter;
 import org.springframework.data.domain.Page;
 
 public interface OrganizationService {
-    public PaginResponse<Organization> find(RequestFilter filter) throws ServiceException;
+    /**
+     * find
+     * @param filter
+     * @return
+     * @throws ServiceException
+     */
+    PaginResponse<Organization> find(RequestFilter filter) throws ServiceException;
+
+    /**
+     * get
+     * @param id
+     * @return
+     * @throws ServiceException
+     */
     Organization get(Long id) throws ServiceException;
+
+    /**
+     * create
+     * @param form
+     * @return
+     * @throws ServiceException
+     */
     Organization create(Organization form) throws ServiceException;
+
+    /**
+     * update
+     * @param form
+     * @param id
+     * @return
+     * @throws ServiceException
+     */
     Organization update(Organization form, Long id) throws ServiceException;
+
+    /**
+     * delete
+     * @param id
+     * @throws ServiceException
+     */
     void delete(Long id) throws ServiceException;
 }
