1dc17e75478df7979d48b7b1f4c1d5003f4902d1
shicham <sass.hicham@gmail.com>
2021-01-11 00:23:57 +0100

    Update OrganizationServiceImpl.java

--- src/main/java/com/crm/api/organization/services/impl/OrganizationServiceImpl.java
+++ src/main/java/com/crm/api/organization/services/impl/OrganizationServiceImpl.java
@@ -54,7 +54,7 @@ public class OrganizationServiceImpl implements OrganizationService {
      */
     @Override
     public PaginResponse<Organization> find(RequestFilter filter) throws ServiceException {
-        Page<OrganizationEntity> page = organizationCustomRepository.find(filter, PageRequest.of(filter.getSize(), filter.getPage()));
+        Page<OrganizationEntity> page = organizationCustomRepository.find(filter, PageRequest.of(filter.getPage(), filter.getSize()));
         return new PaginResponse<>(page.getTotalElements(),organizationMapper.toForm(page.getContent()),filter.getPage(), filter.getSize());
     }
 
